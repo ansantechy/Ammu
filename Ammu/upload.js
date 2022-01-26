ce1306b381df1a2e7943a9f437209765ca3c638b
p.a.t.c.h <90860797+ansantechy@users.noreply.github.com>
2022-01-26 16:06:40 +0530

    Update upload.js

--- Ammu/upload.js
+++ Ammu/upload.js
@@ -4,13 +4,13 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No media found'
+  if (!mime) throw 'No media found😤'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   m.reply(`${link}
 ${media.length} Byte(s)
-${isTele ? '(No Expiration Date)' : '(Not known)'}`)
+${isTele ? '(Succes...ρꪖꪻᥴꫝ Bot)' : '(Not known)'}`)
 }
 handler.help = ['upload']
 handler.tags = ['tools']
